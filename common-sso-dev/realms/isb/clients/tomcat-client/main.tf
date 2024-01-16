13a26961ac0e81b48c06a92d765a334fbb49e77f
Stanley Okeke <Stanley.Okeke@gov.bc.ca>
2024-01-16 01:19:35 -0800

    added more modules

--- common-sso-dev/realms/isb/clients/tomcat-client/main.tf
+++ common-sso-dev/realms/isb/clients/tomcat-client/main.tf
@@ -1,23 +1,10 @@
 
 
-resource "keycloak_openid_client" "CLIENT" {
-  access_token_lifespan               = ""
-  access_type                         = "BEARER-ONLY"
-  backchannel_logout_session_required = true
-  base_url                            = ""
-  client_authenticator_type           = "client-secret"
-  client_id                           = "jam-lea-api"
-  consent_required                    = false
-  description                         = "Law Enforcement Availability API Client"
-  direct_access_grants_enabled        = false
-  enabled                             = true
-  frontchannel_logout_enabled         = false
-  full_scope_allowed                  = false
-  implicit_flow_enabled               = false
-  name                                = "jam-lea-api"
-  pkce_code_challenge_method          = ""
-  realm_id                            = "ISB"
-  service_accounts_enabled            = false
-  standard_flow_enabled               = false
-  use_refresh_tokens                  = true
+module "TOMCAT_OIDC_CLIENT" {
+  source = "../../../../../modules/oidc-client"
+  client_id = "tomcat-client"
+  realm_id = "ISBB"
+  name = "tomcat-client"
+  description = "Tomcat OIDC Client for ORDS"
+  access_type = "CONFIDENTIAL"
 }
